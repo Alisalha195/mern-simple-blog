585ff9a283befa83a1eca0d48aa8c763ef72c3be
alisalha195@gmail.com <alisalha195@gmail.com>
2024-05-30 16:17:40 +0300

    add user card component

--- client/src/admin/components/users/UserCard.jsx
+++ client/src/admin/components/users/UserCard.jsx
@@ -17,7 +17,27 @@ const UserCard = ({user}) => {
 	
 	return (
 		<div className="bg-red-300 mb-2"> 
-			 {user.username}
+			 <div className="flex flex-row">
+			 
+			    {/*...left Box....*/}
+		      <div className="flex flex-col basis-8/12">
+		      </div>
+		      
+		      {/*...Control Buttons....*/}
+		      <div className="flex flex-col basis-4/12 justify-right">
+		          <div className="flex flex-col justify-center">
+		            <span className="px-[5px] py-[2px] [border:1px_solid_#444] bg-[#006600]">
+		              edit
+		            </span>
+		          </div>
+		          
+		          <div>
+		            <span>
+		              delete
+		            </span>
+		          </div>
+		      </div>
+			 </div>
 		</div>
 	)
 }
